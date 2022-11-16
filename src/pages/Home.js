30c144dbf82610408360594fff7fef6b86082402
dathoang <dathoang31082001@gmail.com>
2022-11-16 09:02:02 +0700

    update new file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-const Home = () => (
-  <div>
-    <p className="text-warning">react home</p>
-  </div>
-);
+const Home = () => {
+  return <div>Home</div>;
+};
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    screens: {
+      mobile: "320px",
+      // => @media (min-width: 640px) { ... }
+      bigMobile: "500px",
+      tablet: "640px",
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1400px",
+      lgdesktop: "1490px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
