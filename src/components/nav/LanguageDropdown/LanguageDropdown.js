221b4d85a9a288f4f0b15e3f4e48c75f4c8812b5
dathoang <dathoang31082001@gmail.com>
2022-11-19 21:43:38 +0700

    update mobile nav

---
+++ src/components/nav/LanguageDropdown/LanguageDropdown.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+
+const LanguageDropDown = () => {
+  const items = [
+    {
+      label: (
+        <div
+          onClick={() => setChooseLang("English")}
+          onMouseDown={() => setArrow(!arrow)}
+        >
+          English
+        </div>
+      ),
+      key: "0",
+    },
+    {
+      label: (
+        <div
+          onClick={() => setChooseLang("Vietnamese")}
+          onMouseDown={() => setArrow(!arrow)}
+        >
+          Vietnamese
+        </div>
+      ),
+      key: "1",
+    },
+  ];
+  const [chooseLang, setChooseLang] = useState("English");
+  const [arrow, setArrow] = useState(false);
+
+  return (
+    <Dropdown menu={{ items }} trigger={["click"]}>
+      <div
+        className="flex items-center "
+        onClick={() => setArrow(!arrow)}
+        onMouse={() => setArrow(!arrow)}
+      >
+        <div
+          onClick={(e) => e.preventDefault()}
+          className="tracking-wide font-semibold cursor-pointer"
+        >
+          {chooseLang}
+        </div>
+        <DownOutlined
+          className={`text-[10px] ml-[6px] transition-all duration-300 ${
+            arrow === true ? "rotate-[180deg]" : "rotate-[0deg]"
+          }`}
+        />
+      </div>
+    </Dropdown>
+  );
+};
+
+export default LanguageDropDown;
--- src/components/nav/MobileNavDropDown/MobileNavDropDown.js
+++ src/components/nav/MobileNavDropDown/MobileNavDropDown.js
@@ -2,6 +2,8 @@ import { Menu } from "antd";
 import React, { useState } from "react";
 import "../../../../src/index.scss";
 import { useHistory } from "react-router-dom";
+import LanguageDropdown from "../LanguageDropdown/LanguageDropdown";
+import { BsPercent } from "react-icons/bs";
 
 function GetItem(label, key, children, type, link) {
   return {
@@ -28,27 +30,69 @@ const items = [
 ];
 
 // submenu keys of first level
-const rootSubmenuKeys = ["sub1", "sub2", "sub4"];
+const rootSubmenuKeys = ["sub2", "sub4"];
 const MobileNavDropDown = () => {
-  const [openKeys, setOpenKeys] = useState(["sub1"]);
+  const [openKeys, setOpenKeys] = useState([""]);
+  const [currentLang, setCurrentLang] = useState("en");
+  let history = useHistory();
+
+  const handleLang = (lang) => {
+    setCurrentLang(lang);
+  };
+
   const onOpenChange = (keys) => {
     const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    // console.log("latestOpenKey", latestOpenKey);
+    // console.log("keys", keys);
+    // console.log("openKeys", openKeys);
+    // console.log("latestOpenKey", rootSubmenuKeys.indexOf(latestOpenKey));
     if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
       setOpenKeys(keys);
     } else {
       setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
     }
   };
+  const onSelectChange = (keys2) => {
+    console.log("keys2", keys2.key);
+    const handleNavigate = () => {
+      if (keys2.key === "5") {
+        history.push("/login5");
+      } else if (keys2.key === "6") {
+        history.push("/login6");
+      }
+    };
+    return handleNavigate();
+  };
   return (
-    <Menu
-      mode="inline"
-      openKeys={openKeys}
-      onOpenChange={onOpenChange}
-      style={{
-        width: "100%",
-      }}
-      items={items}
-    />
+    <>
+      <Menu
+        mode="inline"
+        openKeys={openKeys}
+        onOpenChange={onOpenChange}
+        onClick={onSelectChange}
+        style={{
+          width: "100%",
+        }}
+        items={items}
+        selectable={true}
+      />
+      <div className="flex flex-col items-center justify-center py-[15px] bg-white">
+        <div className="flex w-[240px] justify-between">
+          <div className="">
+            <LanguageDropdown />
+          </div>
+          <div className="relative">
+            <div className="w-[0.4px] h-[100%] left-[50%] text-[#5E5C5C] bg-[#5E5C5C] -translate-x-[50%]"></div>
+          </div>
+          <div className="flex items-center ">
+            <BsPercent className="text-[#ff5635] mr-[6px] " />
+            <div className="text-[#ff5635] tracking-wide font-semibold">
+              Clearance Sales
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default MobileNavDropDown;
