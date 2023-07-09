0476a8a67df1de5dc4ad1e287092201b9850816c
dattht.hiday <dathoang31082001@gmail.com>
2023-07-09 20:51:05 +0700

    update code

--- src/components/drawer/SideDrawer.js
+++ src/components/drawer/SideDrawer.js
@@ -5,73 +5,73 @@ import { Link } from "react-router-dom";
 import DrawerItems from "./DrawerItems";
 import { useTranslation } from "react-i18next";
 const SideDrawer = () => {
-  const { t } = useTranslation(["slider"]);
-  const dispatch = useDispatch();
-  const { drawer, cart } = useSelector((state) => ({ ...state }));
+   const { t } = useTranslation(["slider"]);
+   const dispatch = useDispatch();
+   const { drawer, cart } = useSelector((state) => ({ ...state }));
 
-  const textPrice = cart.reduce((currentValue, nextValue) => {
-    return currentValue + nextValue.count * nextValue.price;
-  }, 0);
+   const textPrice = cart.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+   }, 0);
 
-  const USDPrice = textPrice.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
+   const USDPrice = textPrice.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+   });
 
-  const sliderTitle = t("slider.JUST ADDED TO YOUR CART");
+   const sliderTitle = t("slider.JUST ADDED TO YOUR CART");
 
-  return (
-    <Drawer
-      className="text-center"
-      title={sliderTitle}
-      placement="right"
-      closable={false}
-      width={420}
-      onClose={() => {
-        dispatch({
-          type: "SET_VISIBLE",
-          payload: false,
-        });
-      }}
-      open={drawer}
-      footerStyle={{ background: "#f5f5f5" }}
-      footer={
-        <div>
-          <div className="flex justify-between items-center px-[14px]">
-            <div className="p-0 font-bold text-[14px] text-[#666666] tracking-widest">
-              {t("slider.Cart total")}
+   return (
+      <Drawer
+         className="text-center"
+         title={sliderTitle}
+         placement="right"
+         closable={false}
+         width={420}
+         onClose={() => {
+            dispatch({
+               type: "SET_VISIBLE",
+               payload: false,
+            });
+         }}
+         open={drawer}
+         footerStyle={{ background: "#f5f5f5" }}
+         footer={
+            <div>
+               <div className="flex justify-between items-center px-[14px]">
+                  <div className="p-0 font-bold text-[14px] text-[#666666] tracking-widest">
+                     {t("slider.Cart total")}
+                  </div>
+                  <p className="py-[12px]">
+                     <span className="font-bold text-[15px] text-black tracking-wide">
+                        {USDPrice} USD
+                     </span>{" "}
+                     {cart && cart.count}
+                  </p>
+               </div>
+               <Link to="/cart">
+                  <button
+                     onClick={() =>
+                        dispatch({
+                           type: "SET_VISIBLE",
+                           payload: false,
+                        })
+                     }
+                     className="text-center btn w-full button-slide"
+                     style={{ padding: 0 }}
+                  >
+                     <div className="button">{`${t("slider.View Cart")} ( ${
+                        cart.length
+                     } )`}</div>
+                  </button>
+               </Link>
             </div>
-            <p className="py-[12px]">
-              <span className="font-bold text-[15px] text-black tracking-wide">
-                {USDPrice} USD
-              </span>{" "}
-              {cart && cart.count}
-            </p>
-          </div>
-          <Link to="/cart">
-            <button
-              onClick={() =>
-                dispatch({
-                  type: "SET_VISIBLE",
-                  payload: false,
-                })
-              }
-              className="text-center btn w-full button-slide"
-              style={{ padding: 0 }}
-            >
-              <div className="button">{`${t("slider.View Cart")} ( ${
-                cart.length
-              } )`}</div>
-            </button>
-          </Link>
-        </div>
-      }
-    >
-      {cart.map((p) => (
-        <DrawerItems key={p._id} p={p} />
-      ))}
-    </Drawer>
-  );
+         }
+      >
+         {cart.map((p) => (
+            <DrawerItems key={p._id} p={p} />
+         ))}
+      </Drawer>
+   );
 };
 
 export default SideDrawer;
